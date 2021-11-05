0dd90094d23abb3b8ebb2b5fb806a97b5bc73e74
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-11-05 15:52:22 +0900

    moved video files from public folder to src folder, and added heroSection to website

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/pages/Home';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" exact />
+        <Route path="/" exact component={Home}/>
       </Routes>
     </Router>
   );
