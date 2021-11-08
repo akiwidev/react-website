cd5f5907cc054709475427af3e10e6771faf1206
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-11-08 10:37:07 +0900

    updated hero component to show video on homepage

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" exact element={<Home/>}/>
+        <Route path="/" element={<Home/>}/>
       </Routes>
     </Router>
   );
