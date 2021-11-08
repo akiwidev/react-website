f2d10e9c402d33d39ac30ee0a9555425dc65fec1
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-11-08 10:29:46 +0900

    changed component to element in Route path=/ due to Switch being replaced by Routes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import '../../App.css';
-import HeroSection from '../HeroSection';
+import HeroSection from '../HeroSection'
 
 function Home () {
   return (
-    <>
-      <HeroSection />
-    </>
+    <HeroSection />
   );
 }
-
 
 export default Home;
