41d9b712f741110b3968c56ecc8ece855d5f78b5
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-11-09 17:33:30 +0900

    added footer css and then added footer component to app component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/pages/Home';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/products" element={<Products/>}/>
         <Route path="/sign-up" element={<SignUp/>}/>
       </Routes>
+      <Footer />
     </Router>
   );
 }
